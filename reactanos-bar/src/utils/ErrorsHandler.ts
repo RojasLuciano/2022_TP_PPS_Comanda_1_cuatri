6aa0792121590c689be25ff1c550db6f559db5a0
RojasLuciano <lucianorojas@uala.com>
2022-06-20 22:12:27 -0300

    Added vibration

--- reactanos-bar/src/utils/ErrorsHandler.ts
+++ reactanos-bar/src/utils/ErrorsHandler.ts
@@ -1,59 +1,77 @@
 import { showMessage } from 'react-native-flash-message';
+import {  Vibration } from 'react-native';
 
-export const errorHandler = (error:any) => {
-    let message:string="";
-    switch(error){
+export const errorHandler = (error: any) => {
+    let message: string = "";
+    switch (error) {
         case 'auth/invalid-email':
-            message="El correo electrónico es inválido"
+            message = "El correo electrónico es inválido"
+            Vibration.vibrate(1000);
             break;
         case 'auth/email-already-in-use':
-            message="El correo electrónico ingresado ya está registrado"
+            message = "El correo electrónico ingresado ya está registrado"
+            Vibration.vibrate(1000);
             break;
         case 'auth/weak-password':
-            message="La contraseña debe tener un mínimo de 6 carácteres"
-        break;
+            message = "La contraseña debe tener un mínimo de 6 carácteres"
+            Vibration.vibrate(1000);
+            break;
         case 'auth/user-not-found':
-            message="Correo electrónico y/o contraseña inválido"
-        break;
+            message = "Correo electrónico y/o contraseña inválido"
+            Vibration.vibrate(1000);
+            break;
         case 'auth/wrong-password':
-            message="Correo electrónico y/o contraseña inválido"
-        break;
+            message = "Correo electrónico y/o contraseña inválido"
+            Vibration.vibrate(1000);
+            break;
         case 'pass-diff':
-            message="Las contraseñas no coinciden"
-        break;
+            message = "Las contraseñas no coinciden"
+            Vibration.vibrate(1000);
+            break;
         case 'image-error':
-            message="Ha ocurrido un error intentando cargar el producto"
-        break;
+            message = "Ha ocurrido un error intentando cargar el producto"
+            Vibration.vibrate(1000);
+            break;
         case 'empty-fields':
-            message="Todos los campos son requeridos"
-        break;
+            message = "Todos los campos son requeridos"
+            Vibration.vibrate(1000);
+            break;
         case 'table-exists':
-            message="El número de la tabla ya existe"
-        break;
+            message = "El número de la tabla ya existe"
+            Vibration.vibrate(1000);
+            break;
         case 'table-not-exists':
-            message="El código QR no está asociado a alguna de nuestras mesas"
-        break;
+            message = "El código QR no está asociado a alguna de nuestras mesas"
+            Vibration.vibrate(1000);
+            break;
         case 'table-doesnt-match':
-            message="El código QR de esa mesa no es el mismo que la que te asignaron"
-        break;
+            message = "El código QR de esa mesa no es el mismo que la que te asignaron"
+            Vibration.vibrate(1000);
+            break;
         case 'table-taken':
-            message="La mesa ya está ocupada"
-        break;
+            message = "La mesa ya está ocupada"
+            Vibration.vibrate(1000);
+            break;
         case 'unauthorized':
-            message="Usuario no autorizado"
-        break;
+            message = "Usuario no autorizado"
+            Vibration.vibrate(1000);
+            break;
         case 'invalid-qr':
-            message="QR inválido"
-        break;
+            message = "QR inválido"
+            Vibration.vibrate(1000);
+            break;
         case 'havent-guessed':
-            message="No has adivinado el número"
-        break;
+            message = "No has adivinado el número"
+            Vibration.vibrate(1000);
+            break;
         case 'order-error':
-            message="Ocurrió un error al generar el pedido"
-        break;
+            message = "Ocurrió un error al generar el pedido"
+            Vibration.vibrate(1000);
+            break;
         default:
-            message="Ha ocurrido un error, por favor reintente nuevamente"
-        break;
+            message = "Ha ocurrido un error, por favor reintente nuevamente"
+            Vibration.vibrate(1000);
+            break;
     }
-    showMessage({type:"danger", message:"Error", description:message});
+    showMessage({ type: "danger", message: "Error", description: message });
 }
